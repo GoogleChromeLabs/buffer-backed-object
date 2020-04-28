ffb5cef68ef089be9cbb03ce9a17d276ba06da62
Surma <surma@surma.link>
2020-04-28 16:43:39 +0100

    Add TypeScript types

---
+++ buffer-backed-object.type-test.ts
@@ -0,0 +1,27 @@
+import {assert, IsExact} from "conditional-type-checks";
+
+import {BufferBackedObject, ArrayOfBufferBackedObjects} from ".";
+
+const view = new BufferBackedObject(null as any, {
+  id: BufferBackedObject.Uint16({ endianess: "little" }),
+  name: BufferBackedObject.UTF8String(32),
+  data: BufferBackedObject.ArrayBuffer(100),
+  position: BufferBackedObject.NestedBufferBackedObject({
+    x: BufferBackedObject.Float64(),
+    y: BufferBackedObject.Float64(),
+    z: BufferBackedObject.Float64(),
+  })
+});
+
+assert<IsExact<typeof view.id, number>>(true);
+assert<IsExact<typeof view.name, string>>(true);
+assert<IsExact<typeof view.data, ArrayBuffer>>(true);
+assert<IsExact<typeof view.position.x, number>>(true);
+
+const descriptors = {
+  id: BufferBackedObject.Uint16({ endianess: "little" }),
+  name: BufferBackedObject.UTF8String(32),
+};
+const view2 = new ArrayOfBufferBackedObjects(null as any, descriptors);
+
+assert<IsExact<typeof view2, Array<{id: number, name: string}>>>(true);
